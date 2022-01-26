9c2fa78754e4f37a1b468d42f2c2404e033a701b
RashBM <57621825+RashBM@users.noreply.github.com>
2022-01-26 16:23:42 +0000

    footer items

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,35 @@ import styled from "styled-components";
 const Container = styled.div`
   font-size: 0.8rem;
   background: yellow;
-  text-align: center;
   padding-bottom: 10px;
   padding-top: 30px;
   position: relative;
   bottom: -40px;
+  ul {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
   li {
     padding-bottom: 0.5rem;
+  }
+
+  @media (min-width: 768px) {
+    ul {
+      flex-direction: row;
+    }
+
+    a {
+      margin: 0.8rem;
+    }
+  }
+`;
+
+const DesktopItem = styled.li`
+  display: none;
+
+  @media (min-width: 768px) {
+    display: block;
   }
 `;
 
@@ -26,12 +48,18 @@ const Footer = () => {
         <li>
           <a href="#">Privacy & Legal</a>
         </li>
+        <DesktopItem>
+          <a href="#">Contact</a>
+        </DesktopItem>
         <li>
           <a href="#">Careers</a>
         </li>
         <li>
-          <a href="#">News</a>
+          <a href="#">News</a>{" "}
         </li>
+        <DesktopItem>
+          <a href="#">Locations</a>
+        </DesktopItem>
       </ul>
     </Container>
   );
